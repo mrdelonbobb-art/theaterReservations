3014bcc84f71bc0541dfd34dac0ede838cbc290d
delon bobb <mr.delonbobb@gmail.com>
2025-10-04 11:29:18 -0400

    first commit

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -6,7 +6,7 @@ public class TheaterReservations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("please enter your first and last name");
-        String fullname = sc.nextLine().trim();
+        String fullname = sc.nextLine().toUpperCase();
 
         String[] nameParts = fullname.split("\\s+");
 
@@ -25,7 +25,7 @@ String date = sc.nextLine();
  System.out.println("how may tickets do you need?");
  int Tickets = sc.nextInt();
         {if (Tickets <=1)
-    System.out.println("one ticket reserved for "+ date + "under " + fullname);
+    System.out.println("one ticket reserved for "+ date + " under " + fullname);
 else System.out.println(Tickets+" Tickets reserved for "+ date + " under "+ fullname);
 }
 
